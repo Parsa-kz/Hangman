02d846af6c143ea89cfa16a2a89dd6bf3fa13578
Parsa Karimzadeh <skarimzadeh99@gmail.com>
2022-03-31 23:04:13 +0430

    finished

---
+++ script.js
@@ -0,0 +1,63 @@
+const secretPhrases = ["never", "you", "that", "bullet", "break"];
+
+let randomItem = "";
+let clicked = [];
+let result = "";
+let mistakes = 0;
+
+function selectRandomItem() {
+    randomItem = secretPhrases[Math.floor(Math.random() * secretPhrases.length)];
+    document.getElementById("letters").addEventListener("click", buttonHandler);
+    window.addEventListener("keydown", keyHandler);
+}
+
+function setUnderScores() {
+    let splitedWord = randomItem.split("");
+    let mappedWord = splitedWord.map(letter => (clicked.indexOf(letter) >= 0 ? letter : "_"));
+    result = mappedWord.join("");
+    document.getElementById("clue").innerHTML = `<p>${result}</p>`;
+}
+
+function checkedIfWon() {
+    if (randomItem === result) {
+        document.getElementById("gameover").querySelector("p").style.display = "block";
+        document.getElementById("image").querySelector("img").src = "assets/winner.png";
+    }
+}
+
+function checkedIfLost() {
+    if (mistakes === 6) {
+        document.getElementById("gameover").querySelector("p").style.display = "block";
+        document.getElementById("clue").innerHTML = `<p>Random word is ${randomItem}</p>`
+    }
+}
+
+function updateHangmanPic() {
+    const image = document.getElementById("image").querySelector("img");
+    image.src = `assets/hangman${mistakes}.png`;
+}
+
+function letterHandler(letter) {
+    letter = letter.toLowerCase();
+    clicked.indexOf(letter) === -1 ? clicked.push(letter) : null;
+    document.getElementById(letter.toUpperCase()).className = "used";
+    if (randomItem.indexOf(letter) >= 0) {
+        setUnderScores();
+        checkedIfWon();
+    } else if (randomItem.indexOf(letter) === -1) {
+        mistakes++;
+        checkedIfLost();
+        updateHangmanPic();
+    }
+}
+
+function buttonHandler(event) {
+    letterHandler(event.target.id);
+}
+
+function keyHandler(event) {
+    letterHandler(event.key);
+}
+
+selectRandomItem();
+setUnderScores();
